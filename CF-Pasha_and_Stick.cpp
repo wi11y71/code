f5ee28b602b162b368a38f5808e75538fc6a2e7b
wi11y <a0909881559@gmail.com>
2023-09-20 23:57:59 +0800

    ok

---
+++ CF-Pasha_and_Stick.cpp
@@ -0,0 +1,21 @@
+//2023-09-20 23:49:37
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin>>n;
+    if((n%2)){
+        cout<<0;
+        return 0;
+    }
+    n >>= 1;
+    
+    int dis = (!(n%2));
+
+    cout<<n/2-dis;
+
+}
